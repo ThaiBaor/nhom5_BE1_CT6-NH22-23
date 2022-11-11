559e671c63f3d87264020a889e889c4db11545cf
Student <Student@tdc.edu.vn>
2022-11-11 15:12:28 +0700

    First commit

---
+++ models/products.php
@@ -0,0 +1,36 @@
+<?php
+class Products extends Db{
+    public function getAllProducts(){
+        $sql = self::$connection->prepare("SELECT * FROM products ORDER BY `id` DESC");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+
+    public function get3Products(){
+        $sql = self::$connection->prepare("SELECT * FROM `products` ORDER BY `products`.`id` DESC LIMIT 0,3");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+
+    public function get4Products(){
+        $sql = self::$connection->prepare("SELECT * FROM `products` ORDER BY `products`.`id` DESC LIMIT 4,4");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getProductByProtype($type_id){
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE type_id = ?");
+        $sql ->bind_param("i",$type_id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+
+
+}
